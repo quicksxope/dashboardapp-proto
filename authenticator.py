3a67de8fcd5659c4594696cf3e9be386591612b9
quicksxope <145250242+quicksxope@users.noreply.github.com>
2025-05-01 11:00:49 +0700

    Add files via upload

---
+++ authenticator.py
@@ -0,0 +1,13 @@
+import yaml
+import streamlit_authenticator as stauth
+
+with open("config.yaml") as file:
+    config = yaml.safe_load(file)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
